433aa0bdf5888f7caac48c04012f300f28e8557b
Tim <dubovoytima@gmail.com>
2025-05-30 13:50:50 +0200

    Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rapidframework-lib",
-    version="1.0.5",
+    version="1.0.7.2",
     packages=find_packages(),
     install_requires=[
         "msgspec"
